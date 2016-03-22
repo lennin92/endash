4c05bb963a26e352bfc6d9bcd162401a797ebba3
lennin <lennin92@gmail.com>
2016-03-22 15:17:30 -0600

    primera prueba material angular

--- static/dashboard/js/app.js
+++ static/dashboard/js/app.js
@@ -3,4 +3,38 @@
  */
 
 
-angular.module('DashboardApp', ['ngMaterial']);
+var dashboarApp = angular.module('DashboardApp', ['ngMaterial']);
+
+dashboarApp.controller('AppCtrl', function($scope) {
+	    var getImagePath = function() {
+    	if(Math.random()<.5) {
+    		return 'http://dummyimage.com/400x16:9';
+    	}
+    	else {
+    		return 'http://dummyimage.com/225x9:16';
+    	}
+    }
+
+    var partition = function (input, size) {
+        var newArr = [];
+        for (var i = 0; i < input.length; i += size) {
+            newArr.push(input.slice(i, i + size));
+        }
+        return newArr;
+    }
+
+    var photos = [];
+
+    for(var i = 0; i < 100; i++) {
+    	photos.push({
+    		id: i,
+    		path: getImagePath()
+    	})
+    }
+
+    $scope.data = {
+        photos: photos,
+        photos3p: partition(photos, photos.length / 4)
+    };
+
+});
