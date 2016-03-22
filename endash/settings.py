b4e2f424d028750be005d1cc6713be857963fc59
lennin <lennin92@gmail.com>
2016-03-22 13:31:57 -0600

    actualizacion de settings para urls

--- endash/settings.py
+++ endash/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media'
+MEDIA_URL = '/media/'
 STATIC_ROOT = '/home/lennin92/endash/static/'
 MEDIA_ROOT = '/home/lennin92/endash/static/'
 
