1010b5ba89940993646b1288e94123a74958b2cc
lennin <lennin92@gmail.com>
2016-03-23 10:28:59 -0600

    segunda prueba material angular

--- static/dashboard/js/app.js
+++ static/dashboard/js/app.js
@@ -3,15 +3,15 @@
  */
 
 
-var dashboarApp = angular.module('DashboardApp', ['ngMaterial']);
+var dashboardApp = angular.module('DashboardApp', ['ngMaterial']);
 
-dashboarApp.config(function($mdThemingProvider) {
+dashboardApp.config(function($mdThemingProvider) {
   $mdThemingProvider.theme('default')
     .primaryPalette('red')
     .dark();
 });
 
-dashboarApp.controller('AppCtrl', function($scope) {
+dashboardApp.controller('AppCtrl', function($scope) {
     var getImagePath = function() {
     	if(Math.random()<.5) {
     		return '//static/dashboard/img/400x16-9.png';
