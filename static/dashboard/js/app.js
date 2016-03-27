af5674c00650bb0c4612cdb277ad63852ec274d4
lennin <lennin92@gmail.com>
2016-03-27 14:21:16 -0600

    cambios en modelos y serializer para una mejor muestra de datos de las demandas por nodo

--- static/dashboard/js/app.js
+++ static/dashboard/js/app.js
@@ -2,14 +2,14 @@
  * Created by Administrador on 22/03/2016.
  */
 
-var generarGrupos = function (lista, t){
+var generarGrupos = function (lista, t) {
     var l = [];
     var lt = lista.length;
     var i = 0;
-    while(l.length<t) l.push([]);
-    while(lista.length>0){
-        l[i%t].push(lista.pop());
-        i+=1;
+    while (l.length < t) l.push([]);
+    while (lista.length > 0) {
+        l[i % t].push(lista.pop());
+        i += 1;
     }
     return l;
 };
@@ -25,12 +25,12 @@ dashboardApp.config(['$mdThemingProvider', '$routeProvider', '$interpolateProvid
         when('/', {
             templateUrl: '/static/dashboard/views/lista.html',
             controller: 'AppCtrl',
-            controllerAs:'vm'
+            controllerAs: 'vm'
         }).
         when('/nodos/:idNodo', {
             templateUrl: '/static/dashboard/views/nodo.html',
             controller: 'AppCtrl',
-            controllerAs:'vm'
+            controllerAs: 'vm'
         }).
         otherwise({
             redirectTo: '/'
@@ -39,22 +39,53 @@ dashboardApp.config(['$mdThemingProvider', '$routeProvider', '$interpolateProvid
 
 dashboardApp.controller('AppCtrl', ['$scope', '$http', '$window', function ($scope, $http, $window) {
     var vm = this;
-    vm.cargarNodo = function(nodo){
-        $window.location.href='/#/nodos/'+nodo.id;
+    vm.tipoFiltro = 0;
+    vm.cargarNodo = function (nodo) {
+        $window.location.href = '/#/nodos/' + nodo.id;
     };
-    vm.demanda2Text = function(demanda){
+    vm.demanda2Text = function (demanda) {
         var d = new Date(demanda.fecha_fin);
         var m = d.getMonth();
         var s = '';
-        switch (m){
-            case 0: s='/Ene/';break; case 1: s='/Feb/';break;
-            case 2: s='/Mar/';break; case 3: s='/Abr/';break;
-            case 4: s='/May/';break; case 5: s='/Jun/';break;
-            case 6: s='/Jul/';break; case 7: s='/Ago/';break;
-            case 8: s='/Sep/';break; case 9: s='/Oct/';break;
-            case 10: s='/Nov/';break; case 12: s='/Dic/';break;
+        switch (m) {
+            case 0:
+                s = '/Ene/';
+                break;
+            case 1:
+                s = '/Feb/';
+                break;
+            case 2:
+                s = '/Mar/';
+                break;
+            case 3:
+                s = '/Abr/';
+                break;
+            case 4:
+                s = '/May/';
+                break;
+            case 5:
+                s = '/Jun/';
+                break;
+            case 6:
+                s = '/Jul/';
+                break;
+            case 7:
+                s = '/Ago/';
+                break;
+            case 8:
+                s = '/Sep/';
+                break;
+            case 9:
+                s = '/Oct/';
+                break;
+            case 10:
+                s = '/Nov/';
+                break;
+            case 12:
+                s = '/Dic/';
+                break;
         }
-        var txt = 'Al '+ d.getDate() + s + (d.getYear()-100)+'';
+        var txt = 'Al ' + d.getDate() + s + (d.getYear() - 100) + '';
         return txt;
     };
     vm.nodos = [];
@@ -70,4 +101,20 @@ dashboardApp.controller('AppCtrl', ['$scope', '$http', '$window', function ($sco
         vm.nodos = arr;
         vm.grupos = generarGrupos(vm.nodos.slice(), 4);
     });
+    vm.filtrarNodos = function () {
+        var url = '';
+        if (vm.tipoFiltro == 0) url = '/rest-api/nodos/?format=json';
+        else url = '/rest-api/nodos/padres/?format=json';
+
+        $http.get(url).then(function (response) {
+            var res = response.data;
+            var arr = [];
+            for (var i = 0; i < res.length; i++) {
+                if (res[i].fotografia == null) res[i].fotografia = '/static/dashboard/img/none.png';
+                arr.push(angular.extend({}, res[i]));
+            }
+            vm.nodos = arr;
+            vm.grupos = generarGrupos(vm.nodos.slice(), 4);
+        });
+    };
 }]);
