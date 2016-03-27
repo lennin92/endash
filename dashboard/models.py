d75c2b6599a34c28aaa86574cc7768d930ddec2c
lennin <lennin92@gmail.com>
2016-03-27 13:47:33 -0600

    cambios en modelos y serializer para una mejor muestra de datos de las demandas por nodo

--- dashboard/models.py
+++ dashboard/models.py
@@ -6,19 +6,21 @@ import datetime
 
 
 def get_imagen_nodo_dir(obj, filename):
-    return 'nodo_%s/%s'%(obj.id, filename)
+    return 'nodo_%s/%s' % (obj.id, filename)
 
 
-def create_valid_date(y,m,d,h=0,mm=0):
-    if not (y % 4 == 0 and y % 100 != 0 or y % 400 == 0) and m==2 and d==29:
-        return datetime.datetime(y,2,28,h,mm)
+def create_valid_date(y, m, d, h=0, mm=0):
+    if not (y % 4 == 0 and y % 100 != 0 or y % 400 == 0) and m == 2 and d == 29:
+        return datetime.datetime(y, 2, 28, h, mm)
     else:
-        return datetime.datetime(y,m,d,h,mm)
+        return datetime.datetime(y, m, d, h, mm)
+
 
 class DemandaNodo:
     fecha_inicio = None
     fecha_fin = None
     demanda = 0
+
 
 class Nodo(models.Model):
     padre = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name='hijos')
@@ -36,37 +38,44 @@ class Nodo(models.Model):
         dn.fecha_inicio = begin
         dn.fecha_fin = end
         dn.demanda = Medicion.objects.filter(
-            fecha_hora__range=(begin, end)).aggregate(Max('demanda'))['demanda__max']
+                fecha_hora__range=(begin, end),
+                nodo=self
+        ).aggregate(Max('demanda'))['demanda__max']
         a.append(dn)
 
-        end = create_valid_date(end.year, end.month-1, end.day, 23, 59)
+        end = create_valid_date(end.year, end.month - 1, end.day, 23, 59)
         begin = create_valid_date(end.year, end.month, 1)
         dn = DemandaNodo()
         dn.fecha_inicio = begin
         dn.fecha_fin = end
         dn.demanda = Medicion.objects.filter(
-            fecha_hora__range=(begin, end)).aggregate(Max('demanda'))['demanda__max']
+                fecha_hora__range=(begin, end),
+                nodo=self
+        ).aggregate(Max('demanda'))['demanda__max']
         a.append(dn)
 
-        end = create_valid_date(end.year, end.month-1, end.day, 23, 59)
+        end = create_valid_date(end.year, end.month - 1, end.day, 23, 59)
         begin = create_valid_date(end.year, end.month, 1)
         dn = DemandaNodo()
         dn.fecha_inicio = begin
         dn.fecha_fin = end
         dn.demanda = Medicion.objects.filter(
-            fecha_hora__range=(begin, end)).aggregate(Max('demanda'))['demanda__max']
+                fecha_hora__range=(begin, end),
+                nodo=self
+        ).aggregate(Max('demanda'))['demanda__max']
         a.append(dn)
 
         return a
 
     def __str__(self):
-        lg=10
-        if len(self.descripcion)<15: lg=len(self.descripcion)
-        return '[NODO %s] %s: %s'%(self.id, self.nombre, self.descripcion[:lg]+' ...')
+        lg = 10
+        if len(self.descripcion) < 15: lg = len(self.descripcion)
+        return '[NODO %s] %s: %s' % (self.id, self.nombre, self.descripcion[:lg] + ' ...')
 
     class Meta:
         verbose_name = "Nodo"
         verbose_name_plural = "Nodos"
+
 
 class Medicion(models.Model):
     nodo = models.ForeignKey('Nodo')
@@ -80,4 +89,4 @@ class Medicion(models.Model):
         verbose_name_plural = "Mediciones"
 
     def __str__(self):
-        return '[NODO %s] %s %s'%(self.nodo.id, str(self.fecha_hora), str(self.demanda))
+        return '[NODO %s] %s %s' % (self.nodo.id, str(self.fecha_hora), str(self.demanda))
