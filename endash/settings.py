f78194c15545e064763508f9a5964d6258cf9563
lennin <lennin92@gmail.com>
2016-04-01 08:54:32 -0600

    actualizacion para hacer pruebas de sincronizacion

--- endash/settings.py
+++ endash/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_gis',
+    'rest_framework_swagger',
     'dashboard'
 ]
 
--- endash/urls.py
+++ endash/urls.py
@@ -32,5 +32,6 @@ urlpatterns = [
     url(r'^rest-api/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', views.obtain_auth_token),
+    url(r'^docs/', include('rest_framework_swagger.urls')),
     url(r'^$', main),
 ]
