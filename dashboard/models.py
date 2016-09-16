d6c44fa8f754b01cb458e71ad0c7faa34bd03505
lennin <lennin92@gmail.com>
2016-09-15 22:29:03 -0600

    actualizacion de modelos

--- dashboard/models.py
+++ dashboard/models.py
@@ -33,8 +33,17 @@ class Day(models.Model):
 
 
 class Time(models.Model):
-    HOURS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23]
-    MINS  = [0, 15, 30, 45]
+    HOURS = (
+        (0, '0'), (1, '1'), (2, '2'), (3, '3'),
+        (4, '4'), (5, '5'), (6, '6'), (7, '7'),
+        (8, '8'), (9, '9'), (10, '10'), (11, '11'),
+        (12, '12'), (13, '13'), (14, '14'), (15, '15'),
+        (16, '16'), (17, '17'), (18, '18'), (19, '19'),
+        (20, '20'), (21, '21'), (22, '22'), (23, '23'),
+    )
+    MINS  = (
+        (0, '0'), (15, '15'), (30, '30'), (45, '45'),
+    )
     hour = models.IntegerField(blank=False, null=False, choices=HOURS)
     mins = models.IntegerField(blank=False, null=False, choices=MINS)
     char_rep = models.CharField(max_length=5)
